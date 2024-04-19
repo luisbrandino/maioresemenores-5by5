be862f0f76876e33b473d749ea22e36a5123f34b
luisbrandino <luisbrandino.contato@gmail.com>
2024-04-19 15:56:39 -0300

    Add project files.

---
+++ maioresemenores/Program.cs
@@ -0,0 +1,23 @@
+﻿// Faça um programa que receba a idade de 10 pessoas e mostre a quantidade de maiores e menores (sem vetor)
+
+int adults = 0;
+int minors = 0;
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write($"Digite a {i + 1}ª idade: ");
+    int age = int.Parse(Console.ReadLine());
+
+    while (age < 0)
+    {
+        Console.Write($"Idade não pode ser negativa, digite novamente a {i + 1}ª idade: ");
+        age = int.Parse(Console.ReadLine());
+    }
+
+    if (age >= 18)
+        adults++;
+    else
+        minors++;
+}
+
+Console.WriteLine($"Maiores: {adults}\nMenores: {minors}");
